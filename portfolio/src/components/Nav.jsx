439eb93b00a09579cc80ac66695071cadc8c4503
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-25 14:06:08 -0400

    create nav component

---
+++ portfolio/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <div className='nav-container'>
+            <ul className='nav-list'>
+                <li><Link to ='/'>Home</Link></li>
+                <li><Link to ='/about'>About Me</Link></li>
+                <li><Link to ='/projects'>Projects</Link></li>
+                <li><Link to ='/interests'>Interests</Link></li>
+            </ul>    
+        </div>    
+    )
+}
+
+export default Nav;
