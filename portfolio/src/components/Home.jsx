716d0b715d1f0b65d87789ef4f8756b2d80e5a53
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-25 14:26:58 -0400

    fill out home component so can now render in browser

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import photo from '../images/linkedinphoto.jpg';
+
+const Home = () => {
+    return (
+      <div className='home-container'>  
+        <img src = {photo}/>
+        <div className='summary'>
+        <p>I am an aspiring web developer who is looking to make a career change into an exciting field. 
+        For many years, I had regretted not taking computer science courses in school, 
+        knowing how pivotal that field would become to every facet of modern life. 
+        Now, I have decided to pursue this path and am excited to seek these new opportunities!</p>
+        </div>
+      </div>  
+    )
+}
+
+export default Home;
