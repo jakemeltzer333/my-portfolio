141627ec61e74320aeed6616e18ab0ca9c4c1464
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-25 14:08:05 -0400

    make basic footer

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h3>Built by Jacob Meltzer</h3>
+        </div>    
+    )
+}
+
+export default Footer;
