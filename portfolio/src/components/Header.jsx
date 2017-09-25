3a7007d9837781822aa5f5c94f0b1b5aacecdbcc
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-25 14:05:52 -0400

    make header component

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Nav from './Nav.jsx';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>JACOB MELTZER</h1>
+            <Nav />
+        </div>    
+    )
+}
+
+export default Header;
