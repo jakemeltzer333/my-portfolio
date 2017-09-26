2eacb3ed04ac830358200537ae3cd3191456839a
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-26 15:06:00 -0400

    give classnames to elements on interests component

--- portfolio/src/components/Interests.jsx
+++ portfolio/src/components/Interests.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Interests = () => {
     return (
         <div className='interests-container'>
-            <h3>Web Development is Great! But I Have Other Passions as Well! Here are Some Lists of My Favorite Things!</h3>
+            <h3 className='interests-header'>Web Development is Great! But I Have Other Passions as Well! Here are Some Lists of My Favorite Things!</h3>
             <div className='lists'>
-            <ul>
+            <ul className='favorites'>
                 <h4>Favorite Movies of 2017 (so far)</h4>
                 <li>Dunkirk</li>
                 <li>Get Out</li>
@@ -14,7 +14,7 @@ const Interests = () => {
                 <li>Okja</li>    
             </ul>
 
-            <ul>
+            <ul className='favorites'>
                 <h4>Favorite TV Shows of 2017 (so far)</h4>
                 <li>The Leftovers</li>
                 <li>Rick and Morty</li>
@@ -23,7 +23,7 @@ const Interests = () => {
                 <li>Master of None</li>
             </ul>
 
-            <ul>
+            <ul className='favorites'>
                 <h4>Favorite Foods</h4>
                 <li>Ben and Jerry's Ice Cream</li>
                 <li>Sausage Pizza</li>
@@ -32,7 +32,7 @@ const Interests = () => {
                 <li>BBQ Brisket</li>
             </ul>
 
-            <ul>
+            <ul className='favorites'>
                 <h4>Favorite Sports Teams</h4>
                 <li>Minnesota Vikings</li>
                 <li>Minnesota Timberwolves</li>
