80973e21df6bfac9cee0a5d87d51d62f8ecdec5d
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-25 20:35:20 -0400

    add titles for each project above their images

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -9,18 +9,22 @@ const Projects = () => {
     return (
         <div className='projects container'>
             <div className='project-1'>
+                <h3>Back to the Future Trivia</h3>
                 <a href='http://back-to-the-future-trivia.bitballoon.com/'><img src={bttf} alt='BTTF-Trivia'/></a><br/>
                 <a href='https://github.com/jakemeltzer333/Back-To-The-Future-Trivia'><i className="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
             </div>
             <div className='project-2'>
+            <h3>Game of Thrones for Dummies</h3>
                 <a href='http://gameofthronesfordummies.herokuapp.com/'><img src={gotForDummies} alt='GOT-for-dummies'/></a><br/>
                 <a href='https://github.com/jakemeltzer333/Game-of-Thrones-for-Dummies'><i className="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
             </div>
             <div className='project-3'>
+            <h3>LinkGA</h3>
                 <a href='https://vast-temple-53081.herokuapp.com/'><img src={linkGA} alt='linkGA'/></a><br/>
                 <a href='https://github.com/jakemeltzer333/GA-Networking-App'><i className="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
             </div>
             <div className='project-4'>
+            <h3>SeenIt?</h3>
                 <a href='https://have-i-seen-it.herokuapp.com/'><img src={movieSearch} alt='SeenIt'/></a><br/>
                 <a href='https://github.com/jakemeltzer333/HaveISeenThis'><i className="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
             </div>    
