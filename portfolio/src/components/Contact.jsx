ba5ec8d4a8e44f717321204b140ade39b6576cc5
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-26 15:05:23 -0400

    add class names for linkedin and github icons

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
             <h2>Contact Me Here!</h2>
             <i className="fa fa-mobile fa-3x" aria-hidden="true"></i><h3>651-767-2285</h3>
             <i className="fa fa-envelope fa-2x" aria-hidden="true"></i><h3>jmeltzer33@gmail.com</h3>
-            <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i><a href='https://www.linkedin.com/in/jacob-meltzer/'><h3>LinkedIn</h3></a>
-            <i className="fa fa-github-square fa-2x" aria-hidden="true"></i><a href='https://github.com/jakemeltzer333'><h3>GitHub</h3></a>
+            <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i><a className='contact-link' href='https://www.linkedin.com/in/jacob-meltzer/'><h3>LinkedIn</h3></a>
+            <i className="fa fa-github-square fa-2x" aria-hidden="true"></i><a className='contact-link' href='https://github.com/jakemeltzer333'><h3>GitHub</h3></a>
         </div>
     )
 }
